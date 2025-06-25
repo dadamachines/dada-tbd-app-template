95d500bdccf2553d07defdebaf0544b11d8d0777
Robert Manzke <robert.manzke@fh-kiel.de>
2025-06-25 08:12:56 +0200

    removed TODO

--- src/Ui.cpp
+++ src/Ui.cpp
@@ -24,7 +24,6 @@ void Ui::Init(){
     Wire1.begin();
 
     // display init
-    // TODO: Adafruit_SH1106G.cpp in Adafruit library, change l. 139, 140 _page_start_offset = 0 to avoid display line offset!
     softSPI = new SoftwareSPI(OLED_SCLK, OLED_DC, OLED_MOSI);
     display = new DaDa_SSD1309_MCL(128, 64, softSPI, OLED_DC, OLED_RST, OLED_CS); // spec is 10MHz
     display->begin(0, true);
